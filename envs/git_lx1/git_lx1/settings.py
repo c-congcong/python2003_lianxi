3c4d4875a4349f9d6710c378020a2617a8d4d974
e <2812511629@qq.com>
2020-06-28 10:49:42 +0800

    练习的项目

--- envs/git_lx1/git_lx1/settings.py
+++ envs/git_lx1/git_lx1/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'harvw^nx!=uuqvci)5i0%nrfozl9zv^6q3t+@eb%hs5jpwsx02'
+# SECRET_KEY = 'harvw^nx!=uuqvci)5i0%nrfozl9zv^6q3t+@eb%hs5jpwsx02'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
